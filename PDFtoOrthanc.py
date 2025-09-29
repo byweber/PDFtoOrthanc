bba8397a87859666683c38234fa769f5cbbac7ce
Lucas Weber <81442898+byweber@users.noreply.github.com>
2025-09-29 16:24:58 -0300

    PDFtoOrthanc.py

--- PDFtoOrthanc.py
+++ PDFtoOrthanc.py
@@ -1,3 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# PDFtoOrthanc.py — Converte PDF para Orthanc
+#
+# Copyright (C) 2025 Lucas Weber
+#
+# Este programa é software livre: você pode redistribuí-lo e/ou modificá-lo
+# sob os termos da Licença Pública Geral GNU conforme publicada pela Free
+# Software Foundation, na versão 3 da licença ou (a seu critério) qualquer
+# versão posterior.
+#
+# Este programa é distribuído na expectativa de que seja útil, mas **SEM
+# QUALQUER GARANTIA**; sem mesmo a garantia implícita de **COMERCIABILIDADE**
+# ou **ADEQUAÇÃO A UM DETERMINADO FIM**. Consulte a Licença Pública Geral
+# GNU para mais detalhes.
+#
+# Você deve ter recebido uma cópia da Licença Pública Geral GNU junto com
+# este programa. Se não, consulte <https://www.gnu.org/licenses/>.
+#
+
 import os
 import re
 import base64
